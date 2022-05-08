da302ec172e6c3be134eff8fa1b938e1adcb3794
JJJ <971622721@qq.com>
2022-05-08 19:39:06 +0800

    第一次提交

---
+++ cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/DependInfo.cmake
@@ -0,0 +1,90 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/A_doing/rtos_test/RTOS_test1/startup/startup_stm32f103xb.s" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/startup/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/FREERTOS/include"
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/A_doing/rtos_test/RTOS_test1/Core/FREERTOS/croutine.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/croutine.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/FREERTOS/event_groups.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/event_groups.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/FREERTOS/list.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/list.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/FREERTOS/portable/GCC/ARM_CM3/port.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/portable/GCC/ARM_CM3/port.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/FREERTOS/queue.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/queue.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/FREERTOS/stream_buffer.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/stream_buffer.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/FREERTOS/tasks.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/tasks.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/FREERTOS/timers.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/timers.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/Src/gpio.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/Src/gpio.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/Src/main.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/Src/main.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/Src/retarget.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/Src/retarget.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/Src/stm32f1xx_hal_msp.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/Src/stm32f1xx_it.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/Src/syscalls.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/Src/syscalls.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/Src/system_stm32f1xx.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Core/Src/usart.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Core/Src/usart.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "E:/A_doing/rtos_test/RTOS_test1/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "E:/A_doing/rtos_test/RTOS_test1/cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/FREERTOS/include"
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RTOS_test1.elf.dir/cmake_clean.cmake
@@ -0,0 +1,41 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/croutine.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/event_groups.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/list.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/portable/GCC/ARM_CM3/port.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/queue.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/stream_buffer.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/tasks.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/FREERTOS/timers.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/Src/gpio.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/Src/retarget.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Core/Src/usart.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "CMakeFiles/RTOS_test1.elf.dir/startup/startup_stm32f103xb.s.obj"
+  "RTOS_test1.elf"
+  "RTOS_test1.elf.manifest"
+  "RTOS_test1.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/RTOS_test1.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
